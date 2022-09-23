d82694f94ff89f6d9389c3e3e1d2ae86ac096a32
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-23 23:26:35 +0300

    retrieving the news from database using jquery ajax

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
 
     $.getJSON("http://localhost/news-website/backend/new.php", function(data) {
-      console.log(data);
+        const news = document.getElementById("news");
+        for(i=0; i<data.length;i++) {
+            news.innerHTML+=`<div class="card my-3 mx-3" style="width: 22rem">
+                            <h6 class="card-title m-3">${data[i].news_source}</h6>
+                            <div class="card-body">
+                            <h4 class="card-title">${data[i].news_heading}</h4>
+                            <p class="card-text">${data[i].news_content}
+                            </p>
+                            <p class="">${data[i].news_date}</p>
+                            </div>
+                            </div>`
+        }
     });
 });
